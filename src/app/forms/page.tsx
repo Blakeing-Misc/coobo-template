7c59ce272dc78720f3218d86c728dd251d2a1558
Blake Ingenthron <blakeingenthron@gmail.com>
2023-04-03 17:53:06 -0500

    Update page.tsx

--- src/app/forms/page.tsx
+++ src/app/forms/page.tsx
@@ -4,10 +4,8 @@ import React from "react"
 import { useToast } from "@/hooks/use-toast"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Loader2 } from "lucide-react"
-import { SubmitHandler, useForm } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import * as z from "zod"
-
-// import { ValidationError, useForm } from "@formspree/react"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
@@ -27,7 +25,6 @@ const schema = z.object({
 
 function FormsPage() {
   const { toast } = useToast()
-  // const [handleSubmit] = useFormspree("xqkoywwd")
 
   const {
     register,
@@ -84,82 +81,84 @@ function FormsPage() {
           Forms
         </h1>
         <p className="text-lg text-accent-700 sm:text-xl">
-          Styles for headings, paragraphs, lists, etc. Update/optimize font
-          familes by referencing this{" "}
+          Full featured forms using{" "}
           <a
             target="_blank"
             className="underline underline-offset-2 hover:text-accent-500"
-            href="https://nextjs.org/docs/basic-features/font-optimization"
+            href="https://react-hook-form.com/"
           >
-            link
+            React Hook Form
+          </a>
+          ,{" "}
+          <a
+            target="_blank"
+            className="underline underline-offset-2 hover:text-accent-500"
+            href="https://github.com/colinhacks/zod"
+          >
+            Zod Validation
+          </a>
+          , and{" "}
+          <a
+            target="_blank"
+            className="underline underline-offset-2 hover:text-accent-500"
+            href="https://formspree.io/"
+          >
+            Formspree
           </a>
           .
         </p>
         <small className="italic text-gray-500">
-          * To utlize these typography styles each element must be a child of
-          the body class. This can be edited in the global.css file within the
-          styles folder.
+          * To connect to Formspree, you need to create an account and create a
+          new form. Go to the integration tab and copy the endpoint URL. Then,
+          paste it in the .env file as NEXT_PUBLIC_FORMSPREE_ENDPOINT.
         </small>
-        <div className="flex gap-4 ">
-          <a
-            className="mt-4 inline-flex h-10 cursor-pointer items-center justify-center rounded-md bg-slate-900 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2   "
-            href="#section1"
-          >
-            Anchor Tag to H3
-          </a>
-          <a
-            className="mt-4 inline-flex h-10 cursor-pointer items-center justify-center rounded-md bg-slate-900 px-4 py-2 text-sm font-medium text-white transition-colors hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2   "
-            href="#section2"
-          >
-            Anchor Tag to H6
-          </a>
-        </div>
       </section>
       <Separator className="my-4 " />
       <section className="">
-        <div className="body">
-          <h2>Formspree</h2>
-        </div>
+        <form
+          className="max-w-xs space-y-4"
+          onSubmit={handleFormSubmit(onSubmit)}
+        >
+          <div className="grid w-full  items-center gap-1.5">
+            <Label htmlFor="email">Email*</Label>
+            <Input
+              autoComplete="off"
+              id="email"
+              {...register("email")}
+              type="email"
+              name="email"
+            />
+            {errors.email && (
+              <p className="text-sm text-red-500">{errors.email?.message}</p>
+            )}
+          </div>
+
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="message">Your Message*</Label>
+            <Textarea
+              autoComplete="off"
+              {...register("message", { required: true })}
+              id="message"
+              name="message"
+            />
+            {errors.message && (
+              <p className="text-sm text-red-500">{errors.message?.message}</p>
+            )}
+          </div>
+
+          <Button type="submit" disabled={isSubmitting}>
+            {isSubmitting ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                {"Submitting"}
+              </>
+            ) : (
+              "Submit"
+            )}
+          </Button>
+        </form>
+        <pre className="mt-10">{JSON.stringify(watchAllFields, null, 4)}</pre>
       </section>
-
-      <form
-        className="max-w-xs space-y-4"
-        onSubmit={handleFormSubmit(onSubmit)}
-      >
-        {/* register your input into the hook by invoking the "register" function */}
-
-        <div className="grid w-full  items-center gap-1.5">
-          <Label htmlFor="email">Email*</Label>
-          <Input id="email" {...register("email")} type="email" name="email" />
-          {errors.email && (
-            <p className="text-sm text-red-500">{errors.email?.message}</p>
-          )}
-        </div>
-
-        <div className="grid w-full gap-1.5">
-          <Label htmlFor="message">Your Message*</Label>
-          <Textarea
-            {...register("message", { required: true })}
-            id="message"
-            name="message"
-          />
-          {errors.message && (
-            <p className="text-sm text-red-500">{errors.message?.message}</p>
-          )}
-        </div>
-
-        <Button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? (
-            <>
-              <Loader2 className="mr-2 h-4 w-4 animate-spin" />{" "}
-              <span>Submitting</span>
-            </>
-          ) : (
-            "Submit"
-          )}
-        </Button>
-      </form>
-      <pre>{JSON.stringify(watchAllFields, null, 4)}</pre>
     </div>
   )
 }
