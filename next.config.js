9229ef5c71109879325c262645719e6f79748601
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-05 00:05:08 -0500

    update next

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const { withContentlayer } = require("next-contentlayer")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -31,11 +28,15 @@ const nextConfig = {
   },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
-    ignoreBuildErrors: process.env.VERCEL_ENV === "production",
+    ignoreBuildErrors:
+      process.env.VERCEL_ENV === "production" ||
+      process.env.VERCEL_ENV === "staging",
   },
   eslint: {
-    /// Set this to false if you want production builds to abort if there's lint errors
-    ignoreDuringBuilds: process.env.VERCEL_ENV === "production",
+    // Set this to false if you want production builds to abort if there's lint errors
+    ignoreDuringBuilds:
+      process.env.VERCEL_ENV === "production" ||
+      process.env.VERCEL_ENV === "staging",
   },
 }
 
