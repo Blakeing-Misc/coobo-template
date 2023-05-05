9769ade5aa1c9949012bb54e197ed213d251f657
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-05 00:10:30 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -28,15 +28,11 @@ const nextConfig = {
   },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
-    ignoreBuildErrors:
-      process.env.VERCEL_ENV === "production" ||
-      process.env.VERCEL_ENV === "staging",
+    ignoreBuildErrors: true,
   },
   eslint: {
     // Set this to false if you want production builds to abort if there's lint errors
-    ignoreDuringBuilds:
-      process.env.VERCEL_ENV === "production" ||
-      process.env.VERCEL_ENV === "staging",
+    ignoreDuringBuilds: true,
   },
 }
 
