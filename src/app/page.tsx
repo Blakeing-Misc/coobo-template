318eba7321d4ade992b623156fc3709869ebb627
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-05 18:50:49 -0500

    update header slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { siteConfig } from "@/config/site"
 import Header from "@/components/header"
 import HomeSlider from "@/components/home-slider"
 import { SiteHeader } from "@/components/site-header"
+import SwiperSlider from "@/components/swiper-slider"
 import { buttonVariants } from "@/components/ui/button"
 
 export default function HomePage() {
@@ -11,7 +12,8 @@ export default function HomePage() {
     <main>
       {/* <SiteHeader position="fixed" /> */}
       <Header position="fixed" />
-      <HomeSlider />
+      {/* <HomeSlider /> */}
+      <SwiperSlider />
     </main>
   )
 }
