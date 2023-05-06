b1f77c796e4988dd0661115639df25f4568e4c43
Blake Ingenthron <blakeingenthron@gmail.com>
2023-05-06 13:23:29 -0500

    Update swiper-slider.tsx

--- src/components/swiper-slider.tsx
+++ src/components/swiper-slider.tsx
@@ -6,7 +6,10 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/swiper.min.css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
+import Image from "next/image"
 import { SwiperModule } from "swiper/types"
+
+import { Icons } from "@/components/icons"
 
 const modules: SwiperModule[] = Object.values({
   Navigation,
@@ -26,7 +29,60 @@ function SwiperComponent() {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>
+        <div className="absolute h-full w-full ">
+          <div className="container mx-auto h-full lg:grid lg:grid-cols-12 lg:gap-x-8 ">
+            <div className=" flex flex-col items-center justify-center lg:col-span-7 xl:col-span-6">
+              <div className="mx-auto max-w-2xl lg:mx-0">
+                <Icons.logo className="h-11 w-auto" />
+                <div className="hidden sm:mt-32 sm:flex lg:mt-16">
+                  <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-accent-500 ring-1 ring-accent-900/10 hover:ring-accent-900/20">
+                    Anim aute id magna aliqua ad ad non deserunt sunt.{" "}
+                    <a
+                      href="#"
+                      className="whitespace-nowrap font-semibold text-primary-600"
+                    >
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      Read more <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </div>
+                </div>
+                <h1 className="mt-24 text-4xl font-bold tracking-tight text-accent-900 sm:mt-10 sm:text-6xl">
+                  Data to enrich your online business
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-accent-600">
+                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
+                  qui lorem cupidatat commodo. Elit sunt amet fugiat veniam
+                  occaecat fugiat aliqua.
+                </p>
+                <div className="mt-10 flex items-center gap-x-6">
+                  <a
+                    href="#"
+                    className="rounded-md bg-primary-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+                  >
+                    Get started
+                  </a>
+                  <a
+                    href="#"
+                    className="text-sm font-semibold leading-6 text-accent-900"
+                  >
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+              <Image
+                src="https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
+                alt=""
+                className="aspect-[3/2] w-full bg-accent-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+                fill
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
       <SwiperSlide>Slide 2</SwiperSlide>
       <SwiperSlide>Slide 3</SwiperSlide>
       <SwiperSlide>Slide 4</SwiperSlide>
